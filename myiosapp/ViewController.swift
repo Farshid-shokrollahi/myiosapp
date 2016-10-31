ce8ef2ec528ab03c90d6eb90d3c63820a7cb170e
farshid-shokrollahi <f.shokrollahi@hotmail.com>
2016-10-31 23:08:43 +0330

    Initial Commit

---
+++ myiosapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myiosapp
+//
+//  Created by Farshid Shokrollahi on ۱۳۹۵/۸/۱۰ ه‍.ش..
+//  Copyright © ۱۳۹۵ ه‍.ش. Artinweb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
